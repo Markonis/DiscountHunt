e256097c88afbc9a8180fcff8edb1485c503335f
Marko Pavlovic <mare.nis@live.com>
2016-06-30 11:57:48 +0200

    Create DiscountSearchEndpoint

---
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/api/endpoints/DiscountSearchEndpoint.java
@@ -0,0 +1,7 @@
+package rs.elfak.mosis.marko.discounthunt.api.endpoints;
+
+public class DiscountSearchEndpoint extends BaseEndpoint{
+    public DiscountSearchEndpoint() {
+        super("/discount_searches", "discount_search");
+    }
+}
