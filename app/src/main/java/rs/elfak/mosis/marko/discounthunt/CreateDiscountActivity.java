a70adc68fd9a56da1259e44de19afaca9d5607da
Marko Pavlovic <mare.nis@live.com>
2016-06-30 09:53:36 +0200

    Save current location with the discount

--- app/src/main/java/rs/elfak/mosis/marko/discounthunt/CreateDiscountActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/CreateDiscountActivity.java
@@ -1,5 +1,7 @@
 package rs.elfak.mosis.marko.discounthunt;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,12 +24,13 @@ public class CreateDiscountActivity extends AppCompatActivity {
     private Button mCreateButton;
     private EditText mTitle, mDescription, mPrice;
     private Spinner mCategory;
+    private CurrentLocation mCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_discount);
-
+        mCurrentLocation = new CurrentLocation((LocationManager) getSystemService(Context.LOCATION_SERVICE));
         mCreateButton = (Button) findViewById(R.id.create_discount);
         mTitle = (EditText) findViewById(R.id.title);
         mDescription = (EditText) findViewById(R.id.description);
@@ -48,15 +51,17 @@ public class CreateDiscountActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mCategory.setAdapter(adapter);
     }
+
     private void createDiscount() {
         try {
             JSONObject discountJsonObject = new JSONObject();
+            JSONObject userJsonObject = DiscountHunt.currentSession.getJSONObject("user");
+            discountJsonObject.put("user_id", userJsonObject.getInt("id"));
             discountJsonObject.put("title", mTitle.getText().toString());
             discountJsonObject.put("description", mDescription.getText().toString());
             discountJsonObject.put("price", mPrice.getText().toString());
             discountJsonObject.put("category", mCategory.getSelectedItem().toString());
-            JSONObject userJsonObject = DiscountHunt.currentSession.getJSONObject("user");
-            discountJsonObject.put("user_id", userJsonObject.getInt("id"));
+            discountJsonObject.put("location_attributes", mCurrentLocation.toJSONObject());
 
             DiscountEndpoint discountEndpoint = new DiscountEndpoint();
             discountEndpoint.post(discountJsonObject,
