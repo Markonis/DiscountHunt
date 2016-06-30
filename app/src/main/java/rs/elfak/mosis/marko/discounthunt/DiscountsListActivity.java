72c1dec24a8897f0a78ed220a63c608df3a6ddd9
Marko Pavlovic <mare.nis@live.com>
2016-06-30 11:58:27 +0200

    Implement DiscountListActivity basic interaction

--- app/src/main/java/rs/elfak/mosis/marko/discounthunt/DiscountsListActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/DiscountsListActivity.java
@@ -1,27 +1,120 @@
 package rs.elfak.mosis.marko.discounthunt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rs.elfak.mosis.marko.discounthunt.api.endpoints.DiscountSearchEndpoint;
 
 public class DiscountsListActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFab;
+    private Button mSearch;
+    private TextView mCountText, mRadiusText;
+    private EditText mQueryText;
+    private ListView mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discounts_list);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_discount_button);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mList = (ListView) findViewById(R.id.list);
+        mQueryText = (EditText) findViewById(R.id.query_text);
+        mCountText = (TextView) findViewById(R.id.discounts_list_count_text);
+        mRadiusText = (TextView) findViewById(R.id.discounts_list_radius_text);
+
+        mFab = (FloatingActionButton) findViewById(R.id.create_discount);
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startCreateDiscountActivity();
             }
         });
+
+        mSearch = (Button) findViewById(R.id.search);
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
+
+        search();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        search();
+    }
+
+    private void startCreateDiscountActivity() {
+        Intent intent = new Intent(getApplicationContext(), CreateDiscountActivity.class);
+        startActivity(intent);
+    }
+
+    private void search() {
+        String query = mQueryText.getText().toString();
+        JSONObject searchJsonObject = new JSONObject();
+        try {
+            searchJsonObject.put("query", query);
+            DiscountSearchEndpoint endpoint = new DiscountSearchEndpoint();
+            endpoint.post(searchJsonObject,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject searchJsonObject) {
+                        populateList(searchJsonObject);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+        }catch (JSONException ex) {
+
+        }
+    }
+
+    private void populateList(JSONObject searchJsonObject) {
+        ArrayList<String> items = new ArrayList<>();
+        try {
+            String result = searchJsonObject.getString("result");
+            JSONArray jsonArray = new JSONArray(result);
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject discountJsonObject = jsonArray.getJSONObject(i);
+                String itemStr = discountJsonObject.getString("title");
+                items.add(itemStr);
+            }
+        } catch (JSONException e) {}
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        mList.setAdapter(itemsAdapter);
+    }
 }
