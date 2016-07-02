6ef0cffecba7fc89ad6ac8fe1406426edce211f8
Marko Pavlovic <mare.nis@live.com>
2016-07-02 17:28:40 +0200

    Implement UserSearchEndpoint

--- app/src/main/java/rs/elfak/mosis/marko/discounthunt/api/endpoints/UserSearchEndpoint.java
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/api/endpoints/UserSearchEndpoint.java
@@ -3,5 +3,8 @@ package rs.elfak.mosis.marko.discounthunt.api.endpoints;
 /**
  * Created by marko on 7/2/16.
  */
-public class UserSearchEndpoint {
+public class UserSearchEndpoint extends BaseEndpoint {
+    public UserSearchEndpoint() {
+        super("/user_searches", "user_search");
+    }
 }
