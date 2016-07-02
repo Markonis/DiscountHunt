f7bdf9fc64910f3ff21697d25b5d8cd3d48563ae
Marko Pavlovic <mare.nis@live.com>
2016-07-02 17:29:32 +0200

    Implement correct user discovery by MAC address in AddFriendActivity

--- app/src/main/java/rs/elfak/mosis/marko/discounthunt/AddFriendActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/AddFriendActivity.java
@@ -11,7 +11,16 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+
+import rs.elfak.mosis.marko.discounthunt.api.endpoints.UserSearchEndpoint;
 
 public class AddFriendActivity extends AppCompatActivity {
 
@@ -19,8 +28,8 @@ public class AddFriendActivity extends AppCompatActivity {
     private static final int REQUEST_DISCOVERABLE = 2;
     private static final int DISCOVERABLE_DURATION = 300;
 
-    private ArrayList<BluetoothDevice> mDevices;
-    private ArrayAdapter<BluetoothDevice> mListAdapter;
+    private ArrayList<String> mUsers;
+    private ArrayAdapter<String> mListAdapter;
     private ListView mList;
 
     private BluetoothAdapter mBluetoothAdapter;
@@ -36,10 +45,10 @@ public class AddFriendActivity extends AppCompatActivity {
     }
 
     private void initList() {
-        mDevices = new ArrayList<>();
+        mUsers = new ArrayList<>();
         mList = (ListView) findViewById(R.id.list);
-        mListAdapter = new ArrayAdapter<BluetoothDevice>(getApplicationContext(),
-                android.R.layout.simple_list_item_1, mDevices);
+        mListAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, mUsers);
         mList.setAdapter(mListAdapter);
     }
 
@@ -52,7 +61,7 @@ public class AddFriendActivity extends AppCompatActivity {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }else{
+        } else {
             onBlueToothEnabled();
         }
     }
@@ -75,8 +84,7 @@ public class AddFriendActivity extends AppCompatActivity {
                 if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                     // Get the BluetoothDevice object from the Intent
                     BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    // Add the name and address to an array adapter to show in a ListView
-                    mListAdapter.add(device);
+                    onDeviceFound(device);
                 }
             }
         };
@@ -84,23 +92,68 @@ public class AddFriendActivity extends AppCompatActivity {
         // Register the BroadcastReceiver
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-        if(!mBluetoothAdapter.startDiscovery()){
+        if (!mBluetoothAdapter.startDiscovery()) {
             finish();
+        }
+    }
+
+    private void onDeviceFound(BluetoothDevice device) {
+        String address = device.getAddress();
+        UserSearchEndpoint searchEndpoint = new UserSearchEndpoint();
+        JSONObject searchJsonObject = new JSONObject();
+        try {
+            searchJsonObject.put("hardware_uuid", address);
+            searchEndpoint.post(searchJsonObject,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            JSONObject userJsonObject = getUserJson(response);
+                            if (userJsonObject != null) {
+                                addUserToList(userJsonObject);
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+        } catch (JSONException ex) {
+        }
+    }
+
+    private JSONObject getUserJson(JSONObject searchJsonObject) {
+        try {
+            String resultStr = searchJsonObject.getString("result");
+            JSONArray jsonArray = new JSONArray(resultStr);
+            return jsonArray.getJSONObject(0);
+        } catch (JSONException ex) {
+            return null;
+        }
+    }
+
+    private void addUserToList(JSONObject userJsonObject) {
+        try {
+            String fullName = userJsonObject.get("first_name") + " " + userJsonObject.getString("last_name");
+            mListAdapter.add(fullName);
+        } catch (JSONException ex) {
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_ENABLE_BT) {
-            if(resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
                 onBlueToothEnabled();
-            }else{
+            } else {
                 finish();
             }
-        }else if(requestCode == REQUEST_DISCOVERABLE) {
-            if(resultCode == DISCOVERABLE_DURATION){
+        } else if (requestCode == REQUEST_DISCOVERABLE) {
+            if (resultCode == DISCOVERABLE_DURATION) {
                 onDiscoverableEnabled();
-            }else{
+            } else {
                 finish();
             }
         }
