553fe59e45d25d2ad6402f869a3d633d92bd9c52
Marko Pavlovic <mare.nis@live.com>
2016-07-03 00:48:01 +0200

    Create Camera service

---
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/Camera.java
@@ -0,0 +1,40 @@
+package rs.elfak.mosis.marko.discounthunt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class Camera {
+    public static final int CAPTURE_IMAGE_REQUEST_CODE = 1;
+
+    private Activity activity;
+    public Camera(Activity activity){
+        this.activity = activity;
+    }
+
+    public void takePhoto() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        activity.startActivityForResult(cameraIntent, CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    public static String encodeToBase64(Bitmap image)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+    public static Bitmap decodeBase64(String input)
+    {
+        byte[] decodedBytes = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+}
