7e2793b260e0046a879af786111dd00c584872d8
Marko Pavlovic <mare.nis@live.com>
2016-07-04 13:56:28 +0200

    Allow making friendships with already bonded devices

--- app/src/main/java/rs/elfak/mosis/marko/discounthunt/AddFriendActivity.java
+++ app/src/main/java/rs/elfak/mosis/marko/discounthunt/AddFriendActivity.java
@@ -65,7 +65,11 @@ public class AddFriendActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 BluetoothDevice device = mUserDevices.get(position);
-                createBond(device);
+                if(device.getBondState() == BluetoothDevice.BOND_BONDED){
+                    deviceBonded(device);
+                }else{
+                    createBond(device);
+                }
             }
         });
     }
